904f802c03ae02d07417515cac1fc406910bf0ee
sadraliev <a.sadraliev@gmail.com>
2025-05-08 00:40:15 -0500

    feat: add outputs for VPC and subnet IDs to enhance networking visibility

--- session-4/outputs.tf
+++ session-4/outputs.tf
@@ -7,3 +7,13 @@ output "aws_instance_private_ip" {
   value       = aws_instance.private_webserver[*].private_ip
   description = "value of the private ip of the instance"
 }
+output "aws_vpc_id" {
+  value       = aws_vpc.main.id
+  description = "value of the vpc id"
+}
+output "aws_private_subnet_id" {
+  value = aws_subnet.private_subnet[*].id
+}
+output "aws_public_subnet_id" {
+  value = aws_subnet.public_subnet[*].id
+}
